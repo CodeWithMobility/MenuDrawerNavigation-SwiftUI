c5b5f8f0f920ffed17fcecce0b73bb2678287b74
manuaravind <manuaravindpta@gmail.com>
2024-10-29 13:14:38 +0530

    Initial Commit

---
+++ MenuDrawerNavigation-SwiftUI/MenuDrawerNavigation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuDrawerNavigation_SwiftUIApp.swift
+//  MenuDrawerNavigation-SwiftUI
+//
+//  Created by Manu Aravind on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuDrawerNavigation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenuDrawerNavigation-SwiftUIUITests/MenuDrawerNavigation_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MenuDrawerNavigation_SwiftUIUITestsLaunchTests.swift
+//  MenuDrawerNavigation-SwiftUIUITests
+//
+//  Created by Manu Aravind on 29/10/24.
+//
+
+import XCTest
+
+final class MenuDrawerNavigation_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
